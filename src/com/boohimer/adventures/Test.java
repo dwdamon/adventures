11e8faeeec89e66c236d9ad7d802d2d14ea7ea22
dwdamon <dwdamon@gmail.com>
2017-09-24 14:21:59 -0400

    formatting of room text

--- src/com/boohimer/adventures/Test.java
+++ src/com/boohimer/adventures/Test.java
@@ -31,13 +31,13 @@ public class Test implements ITickHandler {
   private void createMaze() {
     //System.out.println(  "Creating your adventure." );
     resolver.addRoom( new Location( "Entrance"
-                                  , "You are in an open field.  To the North there is an entrance to a dreary looking cave.  Might be spiders in there, so be careful if you decide to enter."
+                                  , "You are in an open field.\nTo the North there is an entrance to a dreary looking cave.\nMight be spiders in there, so be careful if you decide to enter."
                                   , "The field you started in.  Freedom awaits in this direction!"
                                   , "north:mainCavern"
                                   ))
             .addRoom( new Location( "mainCavern"
-                                  , "You are in a cave.  You see a bright light to the South.  To the East and West you can see dimly lit tunnels."
-                                  , "A small cave."
+                                  , "You are in a small cave chamber.\nYou see a bright light to the South.\nTo the East and West you can see dimly lit tunnels."
+                                  , "A small cave chamger."
                                   , "south:Entrance,east:dryTunnel,west:wetTunnel"
                                   ))
             .addRoom( new Location( "dryTunnel"
@@ -46,42 +46,42 @@ public class Test implements ITickHandler {
                                   , "north:hallOfBones,west:mainCavern"
                                   ))           
             .addRoom( new Location( "wetTunnel"
-                                  , "You are in a narrow, wet tunnel.  Water drips from the ceiling and it smells dank."
+                                  , "You are in a narrow, wet tunnel.\nWater drips from the ceiling and it smells dank."
                                   , "a wet, narrow tunnel."
                                   , "north:curvingTunnel,east:mainCavern"
                                   ))           
             .addRoom( new Location( "curvingTunnel"
-                                  , "You are in a narrow, curving tunnel.  There is an odd odor from the west."
+                                  , "You are in a narrow, curving tunnel.\nThere is an odd odor from the west."
                                   , "a narrow curving tunnel."
                                   , "south:wetTunnel,east:tinyRoom,west:latrine,up:mossRoom"
                                   ))           
             .addRoom( new Location( "latrine"
-                                  , "You are fairly large room.  It STINKS in here.  The ground revales the truth!  You are in a latrine.  Watch where you step!"
+                                  , "You are fairly large room.\nIt STINKS in here.  The ground reveales the truth!  You are in a latrine.\nWatch where you step!"
                                   , "a farily large room, if a bit smelly."
                                   , "east:curvingTunnel"
                                   ))  
             .addRoom( new Location( "tinyRoom"
-                                  , "You are really small dark room.  There is not much room to stand.  Carved in the wall are the words 'Brainard was here.  Briefly.'"
+                                  , "You are really small dark room.\nThere is not much room to stand.  Carved in the wall are the words 'Brainard was here.  Briefly.'"
                                   , "a tiny dark room."
                                   , "west:curvingTunnel"
                                   ))  
             .addRoom( new Location( "mossRoom"
-                                  , "You are room covered in moss. Hungry? You see a tombstone here that reads: 'Brainard died here.  He ate some moss.'"
+                                  , "You are room covered in moss.\nYou see a tombstone here that reads: 'Brainard died here.  He got hungry and ate some moss.'"
                                   , "a mossy looking room."
                                   , "east:spiderRoom,down:curvingTunnel"
                                   ))  
             .addRoom( new Location( "spiderRoom"
-                                  , "You are room with a giant spider in the corner.  Better creep by."
+                                  , "You are room with a giant spider in the corner.\nBetter creep by."
                                   , "a room with a spider in it."
                                   , "east:non-descriptRoom,west:mossRoom"
                                   ))  
             .addRoom( new Location( "non-descriptRoom"
-                                  , "You are really plain room.  Nothing is particulary significant about it, except for the hole in the ground at the center."
+                                  , "You are really plain room.\nNothing is particulary significant about this room except for the hole in the ground at the center."
                                   , "a non-descript room."
                                   , "west:spiderRoom,down:hallOfBones"
                                   ))  
             .addRoom( new Location( "hallOfBones"
-                                  , "You are room filled with lots and lots of bones.  You hope you don't add yours to the collection."
+                                  , "You are room filled with lots and lots of bones.\nYou hope you don't add yours to the collection."
                                   , "the hall of bones."
                                   , "south:dryTunnel,east:throneRoom,up:non-descriptRoom"
                                   ))
@@ -91,27 +91,27 @@ public class Test implements ITickHandler {
                                   , "east:kingsBoudoir,west:hallOfBones"
                                   ))               
             .addRoom( new Location( "kingsBoudoir"
-                                  , "You are room with a large royal bed in the corner.  It's the kings boudior!  Don't sleep here... he may return.  Besides the porrige is way too cold.\nThere is a tiny hole to the south.  You might be able to fit through it."
+                                  , "You are room with a large royal bed in the corner.\nIt's the kings boudior!  Don't sleep here... he may return.\nBesides the porrige is way too cold.\nThere is a tiny hole to the south.  You might be able to fit through it."
                                   , "the kings boudior."
                                   , "north:kingsPrivy,south:Entrance,west:throneRoom"
                                   ))
             .addRoom( new Location( "kingsPrivy"
-                                  , "You are what appears to be the King's privy.  The place the King does his 'buisness' is to the west.  Shower it to the right. (A shower in a cave?  Who would have thought?)"
+                                  , "You are what appears to be the King's privy.\nThe place the King does his 'buisness' is to the west.  Shower it to the right.\n(A shower in a cave?  Who would have thought?)"
                                   , "the kings privy."
                                   , "north:pennegraphsSojourn,south:kingsBoudoir"
                                   ))
             .addRoom( new Location( "pennegraphsSojourn"
-                                  , "You are in small room.  There is not much here.  There is a chisled carving on the wall with a burning torch next to it.  It reads \"Pennegraph's soujurn begins\""
+                                  , "You are in small room.  There is not much here.\nThere is a chisled carving on the wall with a burning torch next to it.\nThe carving reads \"Pennegraph's soujurn begins\""
                                   , "Pennegraph's soujourn beginning."
                                   , "south:kingsPrivy,east:pennegraphsLastStand"
                                   ))
             .addRoom( new Location( "pennegraphsLastStand"
-                                  , "You are in large field.  Skeletons are scattered about the ground everywhere.  Amongst the skeletons are rusty, decaying weapons of all types.  There was some mighty battle here a long time past.  There is a cave entrance to the west with a carving in the stone that reads \"I Pennegraph will fight no more.  Forever.\""
+                                  , "You are in large field.\nSkeletons are scattered about the ground everywhere.  Amongst the skeletons are rusty, decaying weapons of all types.\nThere was some mighty battle here a long time past.  There is a cave entrance to the west with a carving in the stone that reads \"I Pennegraph will fight no more.  Forever.\""
                                   , "Pennegraph's last stand."
                                   , "east:maurdersTrail"
                                   ))
             .addRoom( new Location( "maurdersTrail"
-                                  , "You are on a narrow trail through the woods.  You can tell that at one time thi was a wider trail.  Perhaps a road.  This is probably the path the marauders ran down Pennegraph and his 'horde of heros'. That battle didn't go too well for the marauders.  But it didnt' go so well for Pennegraph.  Legneds say that only Pennegraph survived that encounter."
+                                  , "You are on a narrow trail through the woods.\nYou can tell that at one time thi was a wider trail.  Perhaps a road.\nThis is probably the path the marauders ran down Pennegraph and his 'horde of heros'. That battle didn't go too well for the marauders.  But it didnt' go so well for Pennegraph.  Legneds say that only Pennegraph survived that encounter."
                                   , "The marauders trail."
                                   , "west:pennegraphsLastStand"
                                   ))                          
